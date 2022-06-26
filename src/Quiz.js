40a03cecacea0924280c4a377c976b1911bf8f4e
dheerajcheerla <69983730+dheerajcheerla@users.noreply.github.com>
2022-06-26 21:04:21 +0530

    Completed Module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component{
     }
     render(){
         return(
-            <div><div classname = "QuizQuestion">{quizData.quiz_questions[0].instruction_test}</div></div>
+            <div> <div classname = "QuizQuestion"> {quizData.quiz_questions[0].instruction_test} </div></div>
         )
 
     }
